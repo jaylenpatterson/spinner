eb612e8aea0adb71070d388f05ddbc20a6e001f3
jaylenpatterson <jaylenpatterson16@gmail.com>
2021-11-30 21:00:19 +0000

    code cleanup

--- spinner2.js
+++ spinner2.js
@@ -1,43 +1,4 @@
 let arr = ['\r|   ', '\r/   ', '\r-   ', '\r\   ', '\r\|   ', '\r\/   ', '\r\-   ', '\r\\   ', '\r\|  '];
-
-
-// setTimeout(() => {
-//   process.stdout.write('\r|   ');
-// }, 100);
-// setTimeout(() => {
-//   process.stdout.write('\r/   ');
-// }, 300);
-
-// setTimeout(() => {
-//   process.stdout.write('\r-   ');
-// }, 500);
-
-// setTimeout(() => {
-//   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r\   ');
-// }, 700);
-
-// setTimeout(() => {
-//   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r\|   ');
-// }, 900);
-// setTimeout(() => {
-//   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r\/   ');
-// }, 1100);
-// setTimeout(() => {
-//   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r\-   ');
-// }, 1300);
-// setTimeout(() => {
-//   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r\\   ');
-// }, 1500);
-// setTimeout(() => {
-//   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r\|  ');
-// }, 1500);
-
 let i = 0;
 let interval = 80;
 let spinner = function () {
